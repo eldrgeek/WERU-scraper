59cb619e5886a4be4a4cd113c7271250c34f4cde
Mike Wolf <mike@embeddedsystemsresearch.org>
2011-06-20 11:45:26 -0400

    Changes to clean up formatting and pave the way to final version

---
+++ state.rb
@@ -0,0 +1,21 @@
+
+def state_save(file, *state)
+	#usage: state_save (var.....)
+	#saves the state in the given file
+  File.open(file, "w") do |file|
+    file.write(YAML::dump(state))
+  end
+end
+
+def state_restore(file) 
+	#usage: a,... = state_restore(file)
+	#restore the variables from the file
+  $/="\n\n"
+  array = []
+  File.open(file, "r") do |object|
+    array =  YAML::load(object)
+  end
+  $/="\n"
+  array
+end
+
--- writer.rb
+++ writer.rb
@@ -1,9 +1,15 @@
-  #Writer: writes the line from the state file:
+  #nWriter: writes the line from the state file:
   #And saves the critical date in werustate.txt.
   #For testing it can be called with a count in which case it will only read the prescribed number of entries.
   #
 require 'state.rb' #needed to save the state
 require 'nokogiri'
+def hourname(hour)
+  return "12am" if hour == 0
+  return "#{hour}am" if hour < 11
+  return "12pm" if hour == 11
+  return "#{hour-11}pm"
+end
 def display_lines( lines )
 puts '['
 lines.each do |line|
@@ -14,67 +20,71 @@ end
 
 
 def write_schedule( lines ) 
-  daynames = %w(Sun, Mon, Tue, Wed, Thu, Fri, Sat )
+  daynames = %w(Sun Mon Tue Wed Thu Fri Sat )
   @builder = Nokogiri::HTML::Builder.new { |doc|
     doc.html {
       doc.head {
-      doc.title "WERU schedule" 
+      doc.title.titleclass "WERU schedule" 
       doc.link(:rel =>"stylesheet",
                :type => "text/css", :href => "weru.css")
-    }
-    doc.body {
-      doc.div.heading.thing! {
-      doc.h1 "WERU Schedule"
-    }
-    doc.table {
-      doc.tr { # top row, with days of week
-      doc.td(:class => 'days') {
-      doc.text 'Time'
-      (0..6).each { |day|
-        doc.text daynames[day]
+      doc.link(:rel =>"stylesheet",
+               :type => "text/css", :href => "base.css")
       }
-    }
-    } #end of top row
+      doc.body {
+        doc.div.heading.thing! {
+        doc.h1 "WERU Schedule"
+      }
+      doc.table {
+        doc.tr { # top row, with days of week
+          doc.td(:class => 'day-heading') {
+            doc.text 'Time'
+          }
+          (0..6).each { |day|
+            doc.td.dayheading {
+              doc.text daynames[day]
+            }
+          }
+        } #end of top row
 
-    doc.tr { #write the row that is the body of the table
-      #write first column with hours
-      doc.td(:class=>'hours'){
-      (0..23).each { |hour|
-        doc.div.hours(:style=>'height: 60px;') {
-          doc.text("Hr:" + hour.to_s) 
-        }
-      }
-    } #finished with the hours column
-    entry = 0
-    (0..7).each { |day| 
-      doc.td(:class => 'days') {
-        puts "going into loop"
-        loop do #we are going to loop until the day changes
-        line =   lines[entry]
-        break if line.nil?
-        #p line
-        name =   line[:name]
-        height = line[:height]
-        genre =  line[:genre]
-        time =   line[:time]
-        doc.div( :class => genre, :style => height){
-           doc.text name
-        }
-        #go to next entry and see if we have switched times or finished
-        entry += 1
-        line = lines[entry]
-        break if line.nil?
-        time = line[:time]
-        p line if time =~ /^12.*am/
-        break if time =~ /^12.*am/
-        end #finish the day..
-      } #finish the td
-    } #loop through the days     
-    } #finish the big row containig it all
-    }#finish the table
-    } #finish the body
-    } #finish the page
-  } #finish the doc
+        doc.tr { #write the row that is the body of the table
+          #write first column with hours
+          doc.td(:class=>'hours'){
+          (0..23).each { |hour|
+            doc.div.hoursdiv {
+              doc.text(hourname(hour)) 
+              }
+            }
+          } #finished with the hours column
+          entry = 0
+          (0..7).each { |day| 
+            doc.td(:class => 'days') {
+              puts "going into loop"
+              loop do #we are going to loop until the day changes
+              line =   lines[entry]
+              break if line.nil?
+              #p line
+              name =   line[:name]
+              height = line[:height]
+              genre =  line[:genre]
+              time =   line[:time]
+              doc.div( :class => "#{genre} show" , :style => height){
+                 doc.text name
+              }
+              #go to next entry and see if we have switched times or finished
+              entry += 1
+              line = lines[entry]
+              break if line.nil?
+              time = line[:time]
+              #p line if time =~ /^12.*am/
+              break if time =~ /^12.*am/
+              end #finish the day..
+            } #finish the td
+          } #loop through the days     
+        } #finish the big row containig it all
+        }#finish the table
+        } #finish the body
+      } #finish the HTML
+    } #finish the doc
     File.open("public/weru.html","w") do |file|
       file.write @builder.to_html
     end
@@ -84,18 +94,28 @@ def map_genres( genres )
   begin
     #read the file of show genres, and the genre category
     File.open("public/genremap.in", "r" ).readlines.each do |line|
+      line.chomp!
       genre,category = line.split('=')
       genres[genre]=category
     end
   rescue
   end
+  genres.keys.each do |genre|
+    #puts" #{genre} :  #{genres[genre]}\n"
+    if genres[genre] == 1
+      puts "Genre not mapped #{genre}\n"
+      genres[genre]=genre 
+    end
+  end
   genres
 end
 
 def get_colormap()
-  category_colors = Hash.new(:white)
+  category_colors = Hash.new(:undefined)
   begin
     File.open("public/categorymap.in", "r").readlines.each do |line|
+      line.chomp!
+      #p line
       category,color = line.split('=')
       category_colors[category] = color
     end
@@ -106,11 +126,13 @@ end
 
 def write_css( genres, category_colors)
   File.open("public/weru.css", "w") do |file|
-    file.write("body {background-color:lightgray}\n")
-    file.write( "td {border: 1px solid darkgray}\n")
     genres.keys.each do |genre| 
       category = genres[genre]
       color = category_colors[category]
+      if color == :undefined
+        color = "white"
+        puts "no color for category #{category}"
+      end
       file.write( ".#{genre} {background-color: #{color}}\n" ) 
     end
   end
@@ -119,8 +141,9 @@ end
   lines,genres = state_restore("werustate.yaml")
   #puts lines
   #puts genres
-  write_schedule(lines)
-  colormap = get_colormap()
-  write_css(genres,colormap)
+  write_schedule(lines); p "scheduled"
+  colormap = get_colormap(); p "colors";# p colormap
+  genres = map_genres(genres); p "genres";# p genres
+  write_css(genres,colormap); p "css"
 
-#vim tabstop=2:softtabstop=2:shiftwidth=2:expandtab
+ #vim tabstop=2:softtabstop=2:shiftwidth=2:expandtab
